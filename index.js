c8e9f8be5179bc9c04549de678205cf0519f3a85
[JKS0905] <[jostei4250@gmail.com]>
2024-04-15 01:05:21 +0200

    work

--- index.js
+++ index.js
@@ -13,18 +13,39 @@ document.addEventListener("DOMContentLoaded", event => {
         });
     }
     // dropdown-content href id
-    scrollToSection("italienskPizzaLink");
+    scrollToSection("italienskPizzaLink1");
+    scrollToSection("italienskPizzaLink2");
     scrollToSection("hvitPizzaLink");
     scrollToSection("calzoneLink");
     scrollToSection("pastaLink");
-    scrollToSection("dressingLink");
-    scrollToSection("drikkeLink");
+    //scrollToSection("dressingLink");
+    //scrollToSection("drikkeLink");
 
 });
 
 
 // Activates menu drop down
 function showSidebar() {
-    const sidebar = document.querySelector(".sidebar")
+    const sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("change");
 }
+
+
+//const menuDropdown = document.querySelector(".nav-bar-item-menu");
+//const dropdownContent = document.querySelector(".dropdown-content");
+//
+//menuDropdown.addEventListener("mouseenter", () => {
+//    dropdownContent.clast.add("show");
+//    dropdownContent.classList.remove("hide"); // Remove the hide class if it was added previously
+//});
+//
+//menuDropdown.addEventListener("mouseleave", () => {
+//    dropdownContent.classList.remove("show");
+//    dropdownContent.classList.add("hide"); // Apply the hide animation
+//});
+//
+//dropdownContent.addEventListener("transitionend", () => {
+//    if (!dropdownContent.classList.contains("show")) {
+//        dropdownContent.classList.remove("hide"); // Remove the hide animation class when transition ends
+//    }
+//});
