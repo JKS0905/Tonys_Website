b5ce50f1bb80a0f0cbe882c39a5d1d6e3329f3c9
[JKS0905] <[jostei4250@gmail.com]>
2024-04-20 01:14:06 +0200

    before merge

--- index.js
+++ index.js
@@ -21,9 +21,14 @@ document.addEventListener("DOMContentLoaded", event => {
 
 // Nav bar dropdown menu events
 const menuText = document.querySelector(".menu-text-arrow");
-const menuArrow = document.querySelector(".menu-text-arrow svg")
+const menuArrow = document.querySelector(".menu-text-arrow svg");
+const navBarItem = document.querySelectorAll(".nav-bar-item");
+const sidebarItem = document.querySelectorAll(".sidebar-item");
 const navBarItemMenu = document.querySelector(".nav-bar-item-menu");
+const navBarItemMenuLink = document.querySelectorAll(".nav-bar-item-menu a");
 const dropdownContent = document.querySelector(".dropdown-content");
+const red = "#b32e31";
+const gray = "#4e4d4d";
 
 menuText.addEventListener("mouseenter", event => {
     dropdownContent.classList.add("open");
@@ -36,17 +41,77 @@ navBarItemMenu.addEventListener("mouseleave", event => {
     menuText.classList.remove("open")
     menuArrow.classList.remove("rotate");
 });
+
 menuText.addEventListener("click", event => {
     dropdownContent.classList.toggle("open");
     menuText.classList.toggle("open")
     menuArrow.classList.toggle("rotate");
 });
 
+navBarItem.forEach(item => {
+    item.addEventListener("touchstart", event => {
+        item.style.color = (red);
+        setTimeout(() => {
+            item.style.color = (gray);
+        },300)
+    });
+});
+sidebarItem.forEach(item => {
+    item.addEventListener("touchstart", event => {
+        item.style.color = (red);
+        setTimeout(() => {
+            item.style.color = (gray);
+        },300)
+    });
+});
+
+navBarItemMenuLink.forEach(item => {
+    item.addEventListener("touchstart", event => {
+        item.style.color = (red);
+        setTimeout(() => {
+            item.style.color = (gray);
+        },300)
+    });
+});
+
+// Menu dropdown event
+menuText.addEventListener("touchstart", event => {
+    event.preventDefault();
+    dropdownContent.classList.toggle("open");
+    menuText.classList.toggle("open")
+    menuArrow.classList.toggle("rotate");
+    navBarItemMenu.classList.add("pressed");
+    if (dropdownContent.classList.contains("open")){
+        navBarItemMenu.style.color = red;
+        navBarItemMenu.style.fill = red;
+    }
+    else {
+        navBarItemMenu.style.color = gray;
+        navBarItemMenu.style.fill = gray;
+    }
+});
+
+document.body.addEventListener("touchstart", event => {
+    // Check if the touch event target is not within the dropdown content
+    if (!dropdownContent.contains(event.target) && !navBarItemMenu.contains(event.target)) {
+        // Close the dropdown
+        dropdownContent.classList.remove("open");
+        menuText.classList.remove("open");
+        menuArrow.classList.remove("rotate");
+        navBarItemMenu.classList.remove("pressed");
+
+        // Reset the styles
+        navBarItemMenu.style.color = gray;
+        navBarItemMenu.style.fill = gray;    
+    }
+});
+
 
 // Activates menu drop down/ Sidebar events
+const sidebar = document.querySelector(".sidebar")
 const sidebarBtn = document.querySelector(".sidebar-button")
-
 
 sidebarBtn.addEventListener("click", event => {
     sidebarBtn.classList.toggle("change");
+    sidebar.classList.toggle("open")
 });
