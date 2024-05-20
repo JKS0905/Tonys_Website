222a42f71b02bac9c61578ff35d80f2abe0f32d7
[JKS0905] <[jostei4250@gmail.com]>
2024-05-20 18:03:53 +0200

    a

--- index.js
+++ index.js
@@ -132,9 +132,9 @@ function handleScroll() {
   }
 }
 
-function openHeader() { gsap.to(header, {duration: 0.2, top: 0, ease: "power1.out" }); }
+function openHeader() { gsap.to(header, {duration: 0.25, top: 0, ease: "power1.out" }); }
 
-function closeHeader() { gsap.to(header, {duration: 0.2, top: -85, ease: "power1.out" }); }
+function closeHeader() { gsap.to(header, {duration: 0.25, top: -80, ease: "power1.out" }); }
 
 function openSidebar() {
     sidebarBtn.classList.add("change");
