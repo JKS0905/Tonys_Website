7552d7f854870eb929626dd42701774c7389b9c0
JKS0905 <141182905+JKS0905@users.noreply.github.com>
2024-05-27 12:38:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -134,9 +134,9 @@ function handleScroll() {
   }
 }
 
-function openHeader() { gsap.to(header, {duration: 0.2, top: 0, ease: "power1.out" }); }
+function openHeader() { gsap.to(header, {duration: 0.3, top: 0, ease: "power1.out" }); }
 
-function closeHeader() { gsap.to(header, {duration: 0.2, top: -85, ease: "power1.out"}); }
+function closeHeader() { gsap.to(header, {duration: 0.3, top: -85, ease: "power1.out"}); }
 
 function openSidebar() {
     sidebarBtn.classList.add("change");
