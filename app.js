5858b71ccb184ba02ca27cacf3b7089fee56aebc
[JKS0905] <[jostei4250@gmail.com]>
2024-08-03 23:22:10 +0200

    test app

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express();
 console.log(`PORT: ${process.env.PORT}`);
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 
 // Custom 404 page
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
+//app.use((req, res) => {
+//  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+//});
 
 // Start the server
 const PORT = process.env.PORT || 8080; // Use environment variable or default to 8080
