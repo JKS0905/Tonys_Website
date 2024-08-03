7e0685264bda8e73f7423e90b17a4867803c0d87
[JKS0905] <[jostei4250@gmail.com]>
2024-08-03 23:45:11 +0200

    modules

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
+require('dotenv').config(); // Load environment variables from .env file
+
 const app = express();
 
-// Debugging output
-console.log(`PORT: ${process.env.PORT}`);
-
 // Serve static files from the 'public' directory
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Custom 404 page
-//app.use((req, res) => {
-//  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-//});
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
 
 // Start the server
-const PORT = process.env.PORT || 8080; // Use environment variable or default to 8080
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
