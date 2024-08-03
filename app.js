8b81130a762db9120e3b20c426edcfed1684f0b6
[JKS0905] <[jostei4250@gmail.com]>
2024-08-03 23:01:47 +0200

    made port privat

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use((req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
