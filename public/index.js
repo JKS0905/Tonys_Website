66342006c93aa7a9b88aa27d147cae824d420a32
[JKS0905] <[jostei4250@gmail.com]>
2024-08-10 19:13:23 +0200

    new animation times

--- public/index.js
+++ public/index.js
@@ -202,8 +202,8 @@ function closeHeader() { gsap.to(header, { duration: 0.25, opacity: 0, top: -70,
 function openSidebar() {
     mainContentWraper.classList.add("blurred");
     sidebarBtn.classList.add("change");
-    //tl.set(sidebar, { opacity: 1 });
-    tl.to(sidebar, { duration: 0.25, top: "auto", ease: "power1.out" });
+    tl.set(sidebar, { opacity: 1 });
+    tl.to(sidebar, { duration: 0.3, top: "auto", ease: "power1.out" });
 }
 
 function closeSidebar() {
@@ -211,10 +211,10 @@ function closeSidebar() {
     sidebarItemMenu.style.fill = gray;
     sidebarBtn.classList.remove("change")
     sidebarMenuArrow.classList.remove("rotate");
-    tl.to(sidebarDropdownContent, { duration: 0.25, height: 0, ease: "power1.out"});
+    tl.to(sidebarDropdownContent, { duration: 0.3, height: 0, ease: "power1.out"});
     tl.set(sidebarDropdownContent, { opacity: 0 });
-    tl.to(sidebar, { duration: 0.25, top: parseFloat(sidebarTopValue), ease: "power1.out"}, "-=0.25");
-    //tl.set(sidebar, { opacity: 0 });
+    tl.to(sidebar, { duration: 0.3, top: parseFloat(sidebarTopValue), ease: "power1.out"}, "-=0.3");
+    tl.set(sidebar, { opacity: 0 });
     mainContentWraper.classList.remove("blurred");
 }
 
@@ -223,14 +223,14 @@ function openSidebarDropdown() {
     sidebarItemMenu.style.fill = red;
     sidebarMenuArrow.classList.add("rotate");
     tl.set(sidebarDropdownContent, { opacity: 1 });
-    tl.to(sidebarDropdownContent, {duration: 0.25, height: "auto", ease: "power1.out"});
+    tl.to(sidebarDropdownContent, {duration: 0.3, height: "auto", ease: "power1.out"});
 }
 
 function closeSidebarDropdown() {
     sidebarItemMenu.style.color = gray;
     sidebarItemMenu.style.fill = gray;
     sidebarMenuArrow.classList.remove("rotate");
-    tl.to(sidebarDropdownContent, { duration: 0.25, height: 0, ease: "power1.out" });
+    tl.to(sidebarDropdownContent, { duration: 0.3, height: 0, ease: "power1.out" });
     tl.set(sidebarDropdownContent, { opacity: 0 });
 }
 
