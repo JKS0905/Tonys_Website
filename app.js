e941f60f091d2b2b9d44208d614c40d1c3e2a252
[JKS0905] <[jostei4250@gmail.com]>
2024-08-24 20:20:46 +0200

    added email to server

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config(); // Load environment variables from .env file
 
 const express = require('express');
+const nodeMailer = require("nodemailer");
 const path = require('path');
-const PORT = process.env.PORT || 8080;
+
+// ENV Vaiabels
+const SERVER_PORT = process.env.SERVER_PORT;
+const TRANSPORTER_PORT = process.env.TRANSPORTER_PORT;
+const TRANSPORTER_HOST = process.env.TRANSPORTER_HOST;
+const TRANSPORTER_USER = process.env.TRANSPORTER_USER;
+const TRANSPORTER_PASSWORD = process.env.TRANSPORTER_PASSWORD;
+const SENDER_EMAIL = process.env.SENDER_EMAIL;
+
 
 const app = express();
 
@@ -14,7 +23,41 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
+function sendEmail() {
+  // SMTP credentials
+  let transporter = nodeMailer.createTransport({
+    host: TRANSPORTER_HOST,
+    port: TRANSPORTER_PORT,
+    secure: false,
+    auth: {
+      user: TRANSPORTER_USER,
+      pass: TRANSPORTER_PASSWORD
+    }
+  });
+
+  // Sender and Reciver information
+  let mailOptions = ({
+    from: SENDER_EMAIL,
+    to: "j.kenneth@hvs.as",
+    subject: "Test from Node.js",
+    text: "Hei denne emailen er send ifra Node.js"
+  });
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`Message sent: %s, ${info.messageId}`);
+    console.log(`Preview URL: %s, ${nodeMailer.getTestMessageUrl(info)}`);
+  }
+)}
+
+
+
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on http://localhost:${SERVER_PORT}`);
 });
+
+sendEmail();
